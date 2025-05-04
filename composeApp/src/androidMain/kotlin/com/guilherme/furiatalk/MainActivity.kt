f548589ff9938afa5104619b486f0361d1aba660
GuilhermeIgnácio <stormierearth9@gmail.com>
2025-05-04 16:58:30 -0300

    Splash Screen

--- composeApp/src/androidMain/kotlin/com/guilherme/furiatalk/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/guilherme/furiatalk/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.guilherme.furiatalk
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
+        installSplashScreen()
 
         setContent {
+
+            val context = LocalContext.current
+            (context as Activity)?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
